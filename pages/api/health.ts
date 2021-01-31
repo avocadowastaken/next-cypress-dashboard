6d93f81ac24d77f21b28e12e41986947f7eb64f8
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-01-31 14:58:30 +0500

    feat(api): [health] optimize syntax.

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -2,13 +2,13 @@ import { prisma } from "api/db";
 import { createRequestHandler } from "api/http/RequestHandler";
 
 export default createRequestHandler(async (req, res) => {
+  const [projectsCount, lastProject] = await Promise.all([
+    prisma.project.count(),
+    prisma.project.findFirst({ orderBy: { createdAt: "desc" } }),
+  ]);
+
   res.json({
     request: { method: req.method },
-    projects: {
-      count: await prisma.project.count(),
-      last: await prisma.project.findFirst({
-        orderBy: { createdAt: "desc" },
-      }),
-    },
+    projects: { count: projectsCount, last: await lastProject },
   });
 });
