c2abc7dbc342381bbbb2287030f5d6cade76cb7c
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-08 10:35:56 +0500

    feat(api): [auth] Add GitHub OAuth. (#7)

--- src/api/env.ts
+++ src/api/env.ts
@@ -3,3 +3,17 @@ function resolveKey<T extends string | null>(key: string, defaultValue: T): T {
 }
 
 export const CYPRESS_RECORD_KEY = resolveKey("CYPRESS_RECORD_KEY", null);
+
+export function env(key: string): string {
+  if (!(key in process.env)) {
+    throw new Error(`env: '${key}' is not defined`);
+  }
+
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`env: '${key}' is empty`);
+  }
+
+  return value;
+}
