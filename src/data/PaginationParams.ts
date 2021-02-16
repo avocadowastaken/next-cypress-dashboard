4b0d287fa0fd6acb2f4d38badbb8949f7a97dd41
Umidbek Karimov <uma.karimov@gmail.com>
2021-02-17 02:11:08 +0500

    feat(app): [ProjectsPage] use `Pagination` component.

--- src/data/PaginationParams.ts
+++ src/data/PaginationParams.ts
@@ -4,21 +4,8 @@ export function toPageParam(input: unknown): number {
   }
 
   if (typeof input == "number" && Number.isFinite(input)) {
-    return Math.max(0, Math.round(input));
+    return Math.max(1, Math.round(input));
   }
 
-  return 0;
-}
-
-export function toRowsPerPageParam(
-  input: unknown,
-  rowsPerPageOptions: readonly number[]
-): number {
-  if (typeof input == "string") {
-    input = parseInt(input, 10);
-  }
-
-  return typeof input === "number" && rowsPerPageOptions.includes(input)
-    ? input
-    : rowsPerPageOptions[0];
+  return 1;
 }
