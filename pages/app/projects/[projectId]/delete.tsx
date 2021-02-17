3f766d29869271f692d30d836288b9c14214489d
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-17 12:22:30 +0500

    feat(app): [ProjectsPage] show success messages.

--- pages/app/projects/[projectId]/delete.tsx
+++ pages/app/projects/[projectId]/delete.tsx
@@ -2,7 +2,11 @@ import { prisma } from "@/api/db";
 import { AppTitle } from "@/app/AppLayout";
 import { createServerSideProps } from "@/app/data/ServerSideProps";
 import {
-  Alert,
+  AppErrorCode,
+  extractErrorCode,
+  formatErrorCode,
+} from "@/shared/AppError";
+import {
   Button,
   Dialog,
   DialogActions,
@@ -15,9 +19,8 @@ import { Project } from "@prisma/client";
 import NextLink from "next/link";
 
 interface DeleteProjectPageProps {
-  error?: string;
   project: Project;
-  removed?: boolean;
+  error?: AppErrorCode;
 }
 
 export const getServerSideProps = createServerSideProps<
@@ -35,29 +38,26 @@ export const getServerSideProps = createServerSideProps<
       const deleteConfirmation = query.confirmation;
 
       if (typeof deleteConfirmation == "string") {
+        if (deleteConfirmation != `${project.org}/${project.repo}`) {
+          return { props: { project, error: "BAD_REQUEST" } };
+        }
+
         try {
-          const expectedName = `${project.org}/${project.repo}`;
-
-          if (deleteConfirmation != expectedName) {
-            throw new Error(`Please type ${expectedName} to confirm.`);
-          }
-
           await prisma.project.update({
             where: { id: projectId },
             data: { users: { disconnect: { id: userId } } },
           });
 
-          return { props: { project, removed: true } };
-        } catch (error) {
           return {
-            props: {
-              project,
-              error:
-                error instanceof Error
-                  ? error.message
-                  : "Failed to remove a project",
+            redirect: {
+              permanent: false,
+              destination: `/app/projects?success=${encodeURIComponent(
+                `${project.org}/${project.repo} removed`
+              )}`,
             },
           };
+        } catch (error) {
+          return { props: { project, error: extractErrorCode(error) } };
         }
       }
 
@@ -71,7 +71,6 @@ export const getServerSideProps = createServerSideProps<
 export default function DeleteProjectPage({
   error,
   project,
-  removed,
 }: DeleteProjectPageProps) {
   return (
     <Dialog open={true}>
@@ -84,56 +83,48 @@ export default function DeleteProjectPage({
         ]}
       />
 
-      {removed ? (
-        <Alert
-          action={
-            <NextLink replace={true} passHref={true} href="/app/projects">
-              <Button color="inherit">Close</Button>
-            </NextLink>
-          }
-        >
-          {project.org}/{project.repo} removed
-        </Alert>
-      ) : (
-        <form>
-          <DialogContent>
-            <Grid container={true} spacing={2}>
-              <Grid item={true} xs={12}>
-                This action cannot be undone. This will permanently revoke your
-                access to the{" "}
-                <Typography color="primary">
-                  {project.org}/{project.repo}
-                </Typography>{" "}
-                project.
-              </Grid>
-
-              <Grid item={true} xs={12}>
-                <TextField
-                  required={true}
-                  autoFocus={true}
-                  fullWidth={true}
-                  error={!!error}
-                  helperText={error}
-                  name="confirmation"
-                  label="Please type project name"
-                />
-              </Grid>
+      <form>
+        <DialogContent>
+          <Grid container={true} spacing={2}>
+            <Grid item={true} xs={12}>
+              This action cannot be undone. This will permanently revoke your
+              access to the{" "}
+              <Typography color="primary">
+                {project.org}/{project.repo}
+              </Typography>{" "}
+              project.
             </Grid>
-          </DialogContent>
 
-          <DialogActions>
-            <NextLink
-              replace={true}
-              passHref={true}
-              href={`/app/projects/${project.id}`}
-            >
-              <Button>Dismiss</Button>
-            </NextLink>
+            <Grid item={true} xs={12}>
+              <TextField
+                required={true}
+                autoFocus={true}
+                fullWidth={true}
+                error={!!error}
+                name="confirmation"
+                label="Please type project name"
+                helperText={
+                  error === "BAD_REQUEST"
+                    ? `Please type ${project.org}/${project.repo} to confirm`
+                    : error && formatErrorCode(error)
+                }
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
 
-            <Button type="submit">Confirm</Button>
-          </DialogActions>
-        </form>
-      )}
+        <DialogActions>
+          <NextLink
+            replace={true}
+            passHref={true}
+            href={`/app/projects/${project.id}`}
+          >
+            <Button>Dismiss</Button>
+          </NextLink>
+
+          <Button type="submit">Confirm</Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
