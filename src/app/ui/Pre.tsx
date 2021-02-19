808f4782ab70d97ac645ab72229f65db082bd8a6
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-19 14:31:23 +0500

    fix(app): [Pre] disable auto select.

--- src/app/ui/Pre.tsx
+++ src/app/ui/Pre.tsx
@@ -1,21 +1,12 @@
-import { ReactNode } from "react";
+import { ReactElement, ReactNode } from "react";
 
 export interface PreProps {
   children?: ReactNode;
 }
 
-export function Pre({ children }: PreProps) {
+export function Pre({ children }: PreProps): ReactElement {
   return (
-    <pre
-      onClick={(event) => {
-        const range = new Range();
-        range.setStart(event.currentTarget, 0);
-        range.setEnd(event.currentTarget, 1);
-
-        document.getSelection()?.removeAllRanges();
-        document.getSelection()?.addRange(range);
-      }}
-    >
+    <pre>
       <code>{children}</code>
     </pre>
   );
