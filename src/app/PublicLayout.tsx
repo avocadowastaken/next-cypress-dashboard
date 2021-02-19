577534d5f959e2f103051a5ea5c6985b7cbacd60
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-19 12:06:00 +0500

    fix(app): [HomePage] use `next/head` to define title.

--- src/app/PublicLayout.tsx
+++ src/app/PublicLayout.tsx
@@ -1,15 +1,26 @@
 import { AppBar, Container, Grid, Toolbar } from "@material-ui/core";
 import { LoadingButton } from "@material-ui/lab";
+import Head from "next/head";
 import NextLink from "next/link";
 import React, { ReactElement, ReactNode } from "react";
 
 export interface PublicLayoutProps {
+  title?: string;
   children?: ReactNode;
 }
 
-export function PublicLayout({ children }: PublicLayoutProps): ReactElement {
+export function PublicLayout({
+  title,
+  children,
+}: PublicLayoutProps): ReactElement {
   return (
     <>
+      {!!title && (
+        <Head>
+          <title>{title}</title>
+        </Head>
+      )}
+
       <AppBar position="sticky">
         <Toolbar>
           <Grid container={true} justifyContent="flex-end">
