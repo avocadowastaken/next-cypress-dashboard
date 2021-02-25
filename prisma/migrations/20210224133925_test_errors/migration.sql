e4c6996f8387f3eed08f55f8f069d7304a307666
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-02-25 19:09:44 +0500

    feat: Delete Prisma migrations directory.

--- prisma/migrations/20210224133925_test_errors/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "TestResultState" ADD VALUE 'skipped';
-
--- AlterTable
-ALTER TABLE "RunInstance" ALTER COLUMN "error" SET DATA TYPE TEXT;
-
--- AlterTable
-ALTER TABLE "TestResult" ALTER COLUMN "displayError" SET DATA TYPE TEXT;
