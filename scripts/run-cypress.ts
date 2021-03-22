438e8b61573dd167087587d18fdf2661d809644f
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-03-22 23:05:46 +0500

    chore: Bump dependencies.

--- scripts/run-cypress.ts
+++ scripts/run-cypress.ts
@@ -1,6 +1,31 @@
 import { exec } from "@actions/exec";
+import { parse } from "dotenv";
+import { readFileSync } from "fs";
+import * as path from "path";
+
+function getRecordKey(): string {
+  const { CYPRESS_RECORD_KEY } = process.env;
+
+  if (CYPRESS_RECORD_KEY) {
+    return CYPRESS_RECORD_KEY;
+  }
+
+  const dotEnvFilePath = path.join(__dirname, "..", ".env");
+
+  try {
+    const dotEnvFileContent = readFileSync(dotEnvFilePath, "utf8");
+    const { TASKS_API_SECRET } = parse(dotEnvFileContent);
+
+    if (TASKS_API_SECRET) {
+      return TASKS_API_SECRET;
+    }
+  } catch {}
+
+  return "";
+}
 
 async function main() {
+  const recordKey = getRecordKey();
   const [id = "id1"] = process.argv.slice(2);
 
   await exec(
@@ -9,7 +34,7 @@ async function main() {
     {
       env: {
         ...process.env,
-        CYPRESS_RECORD_KEY: "123",
+        CYPRESS_RECORD_KEY: recordKey,
         DEBUG: "cypress:server:record,cypress:server:api",
       },
     }
