49d8512c80ae183e43902128f0aed248da57efe1
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-04-09 01:51:47 +0500

    WIP

--- src/core/secrets.ts
+++ src/core/secrets.ts
@@ -1,15 +1,15 @@
-function resolveRequired(key: string): string {
+function resolve(key: string): string {
   const value = process.env[key];
 
   if (!value) {
-    throw new Error(`env: '${key}' is empty`);
+    throw new Error(`secret: '${key}' is empty`);
   }
 
   return value;
 }
 
-export const SESSION_SECRET = resolveRequired("SESSION_SECRET");
-export const TASKS_API_SECRET = resolveRequired("TASKS_API_SECRET");
+export const SESSION_SECRET = resolve("SESSION_SECRET");
+export const TASKS_API_SECRET = resolve("TASKS_API_SECRET");
 
-export const GITHUB_CLIENT_ID = resolveRequired("GITHUB_ID");
-export const GITHUB_CLIENT_SECRET = resolveRequired("GITHUB_SECRET");
+export const GITHUB_CLIENT_ID = resolve("GITHUB_ID");
+export const GITHUB_CLIENT_SECRET = resolve("GITHUB_SECRET");
